0e18189aceb01e83e5355ef465283892430eb1f6
Victoria Green <greenv2@tcnj.edu>
2018-04-20 16:25:54 -0400

    added comments and corrected bullet points and numbering

--- code/config/initializers/wicked_pdf.rb
+++ code/config/initializers/wicked_pdf.rb
@@ -15,7 +15,7 @@ WickedPdf.config = {
    #exe_path:'/home/student1/.gem/ruby/2.3.0/wkhtmltopdf'
   #   or
   exe_path: Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf')
-  :disposition =>'attachment'
+
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
